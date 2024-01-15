9a2cb3f956a601733cdd3427ba92abff3d97efdf
Franiboy <franiboy@web.de>
2024-01-15 08:39:36 +0100

    added style to RoomsSelection

--- frontend/src/components/RoomsSelection.js
+++ frontend/src/components/RoomsSelection.js
@@ -5,19 +5,27 @@ const RoomsSelection = ({
   availableRooms,
 }) => {
   return (
-    <select
-      value={choosenRoom}
-      onChange={(e) => setChoosenRoom(e.target.value)}
-      style={{
-        width: "fit-content",
-      }}
-    >
-      {availableRooms.map((ele) => (
-        <option key={ele} value={ele}>
-          {ele} {ele == currentActiveRoom && "(Aktiv)"}
-        </option>
-      ))}
-    </select>
+    <div style={{ fontSize: 17, fontWeight: 500, color: "white" }}>
+      <>Raum: </>
+      <select
+        value={choosenRoom}
+        onChange={(e) => setChoosenRoom(e.target.value)}
+        style={{
+          width: "fit-content",
+          fontSize: 17,
+          fontWeight: 500,
+          marginLeft: 10,
+          padding: 5,
+          borderRadius: 10,
+        }}
+      >
+        {availableRooms.map((ele) => (
+          <option key={ele} value={ele}>
+            {ele} {ele == currentActiveRoom && "(Aktiv)"}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
