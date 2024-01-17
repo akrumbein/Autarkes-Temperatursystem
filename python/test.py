4bf8fc6426f84209bde5c54161bd6706cd14c7a6
Franiboy <franiboy@web.de>
2024-01-17 10:45:02 +0100

    asd

--- python/test.py
+++ python/test.py
@@ -3,7 +3,7 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import ST7735
 disp = ST7735.ST7735(port=0, cs=0, dc="GPIO25", backlight=None,
-rst="GPIO24", width=125, height=150, rotation=0)
+rst="GPIO24", width=125, height=155, rotation=0)
 
 # Initialize display.
 disp.begin()
@@ -17,7 +17,7 @@ print(WIDTH)
 # Load default font.
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 16)
 
-text = "better\n than\n nothing\n"
+text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
 
 img = Image.new('RGB', (WIDTH, HEIGHT))
 
