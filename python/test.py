fba941991a83f0e3fc920717713e125437fc2c1a
Franiboy <franiboy@web.de>
2024-01-17 10:47:53 +0100

    asdas

--- python/test.py
+++ python/test.py
@@ -2,11 +2,19 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import ST7735
+import sys
+
+if len(sys.argv) > 1:
+    text = sys.argv[1]
+else:
+    exit()
+
 disp = ST7735.ST7735(port=0, cs=0, dc="GPIO25", backlight=None,
-rst="GPIO24", width=125, height=155, rotation=0)
+rst="GPIO24", width=125, height=155, rotation=90)
 
 # Initialize display.
 disp.begin()
+
 
 
 WIDTH = disp.width
@@ -17,12 +25,9 @@ print(WIDTH)
 # Load default font.
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 16)
 
-text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-
 img = Image.new('RGB', (WIDTH, HEIGHT))
 
 draw = ImageDraw.Draw(img)
-
 
 draw.text((5,5), text, font=font, fill=(255, 255, 255))
 disp.display(img)
