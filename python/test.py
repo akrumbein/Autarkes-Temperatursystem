8599911906a99938da98e7d230f11079105eaaf1
Franiboy <franiboy@web.de>
2024-01-17 09:57:06 +0100

    asda

--- python/test.py
+++ python/test.py
@@ -20,18 +20,7 @@ draw = ImageDraw.Draw(img)
 
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 30)
 
-x1, y1, x2, y2 = font.getbbox("SIEHT MAN DAS?")
-size_x = x2 - x1
-size_y = y2 - y1
-
-text_x = 160
-text_y = (80 - size_y) // 2
-
-t_start = time.time()
-
 while True:
-    x = (time.time() - t_start) * 100
-    x %= (size_x + 160)
     draw.rectangle((0, 0, 160, 80), (0, 0, 0))
-    draw.text((int(text_x - x), text_y), "SIEHT MAN DAS?", font=font, fill=(255, 255, 255))
+    draw.text((5, 5), "SIEHT MAN DAS?", font=font, fill=(255, 255, 255))
     disp.display(img)
