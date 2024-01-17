2032755099292149fd6eba32bc789e3d3c2724c5
Franiboy <franiboy@web.de>
2024-01-17 10:28:22 +0100

    asd

--- python/test.py
+++ python/test.py
@@ -1,9 +1,9 @@
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-import st7735
-disp = st7735.ST7735(port=0, cs=0, dc="GPIO25", backlight=None,
-rst="GPIO24", width=128, height=128, rotation=0)
+import ST7735
+disp = ST7735.ST7735(port=0, cs=0, dc="GPIO25", backlight=None,
+rst="GPIO24", width=128, height=160, rotation=0)
 
 # Initialize display.
 disp.begin()
